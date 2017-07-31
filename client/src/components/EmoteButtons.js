198798298e9b6a572cd88cf0c608ee1619dc1dda
dustwise <jared.mohney@gmail.com>
2017-07-31 07:28:39 -0600

    subscriber emotes and global emotes now shown on client

--- client/src/components/EmoteButtons.js
+++ client/src/components/EmoteButtons.js
@@ -6,23 +6,69 @@ import styled from 'styled-components';
 const ButtonWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  flex-flow: column;
 `;
 
 const Button = styled.button`
 `;
 
 class EmoteButtons extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      subEmotes : [],
+      globalEmotes : []
+    }
+  }
+
+  componentWillMount(){
+    this.divideEmotes();
+  }
+
+  divideEmotes(){
+    const subEmotes = [];
+    const globalEmotes = [];
+
+    this.props.library.forEach(emote => {
+      if(emote.moments.length > 5){
+        
+        emote.channelEmote
+        ?
+          subEmotes.push(emote)
+        :
+          globalEmotes.push(emote)
+      }
+    })
+
+    this.setState({
+      subEmotes,
+      globalEmotes
+    });
+  }
+
+  generateRow(emotes){
+    return emotes.map(emote => {
+      return (
+        <Button key={emote.name} onClick={() => this.props.dispatch(updateActive({activeEmote: emote.name}))}>
+          <img src={`https://static-cdn.jtvnw.net/emoticons/v1/${emote.imgID}/1.0`} alt={emote}/>
+        </Button>
+      ) 
+    })
+  }
+
+
   render() {
     return (
       <ButtonWrapper>
-        {this.props.library.map(emote => {
-          return(
-            <Button key={emote.name} onClick={() => this.props.dispatch(updateActive({activeEmote: emote.name}))}>
-              <img src={`https://static-cdn.jtvnw.net/emoticons/v1/${emote.imgID}/1.0`} alt={emote}/>
-            </Button>
-          ) 
-            
-        })}
+        <h3>Subscriber Emotes</h3>
+          <div>
+            {this.generateRow(this.state.subEmotes)}
+          </div>
+        <h3>Global Emotes</h3>
+          <div>
+            {this.generateRow(this.state.globalEmotes)}
+          </div>
       </ButtonWrapper>
     );
   }
